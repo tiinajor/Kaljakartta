62cb9bbe4c025da3e46b7818ad7a18835ceb2a7b
Mirko Nowak <microvvavve@gmail.com>
2017-08-28 20:12:36 +0300

    Added sliders and blur effect when the side menu is open

--- js/script.js
+++ js/script.js
@@ -1,5 +1,63 @@
+window.onload = function(){
+  const priceSlider = document.getElementById('price-slider');
+  const alcoholSlider = document.getElementById('alcohol-slider');
+  const distanceSlider = document.getElementById('distance-slider');
+
+
+  noUiSlider.create(priceSlider, {
+    start: [ 0, 8.5 ],
+    connect: true,
+    behaviour: 'tap-drag',
+    tooltips: true,
+    step: 0.5,
+    range: {
+      'min': [  0 ],
+      'max': [ 25 ]
+    },
+    format: wNumb({
+      decimals: 1,
+      postfix: '€',
+    })
+  });
+
+  noUiSlider.create(alcoholSlider, {
+    start: [ 0, 4.7 ],
+    connect: true,
+    behaviour: 'tap-drag',
+    tooltips: true,
+    range: {
+      'min': [ 0 ],
+      'max': [ 12 ]
+    },
+    format: wNumb({
+      decimals: 1,
+      postfix: '%',
+    })
+  });
+
+    noUiSlider.create(distanceSlider, {
+    start: 500,
+    connect: [true, false],
+    behaviour: 'tap-drag',
+    tooltips: true,
+    range: {
+      'min': [  0, 50 ],
+      '50%': [  1000, 500 ],
+      'max': [ 5000 ]
+    },
+    format: wNumb({
+      decimals: 0,
+      postfix: 'm',
+    })
+  });
+
+
+}
+
+
+/* luo kartan */
 function initMap() {
-  var position = {lat: 60.170618, lng: 24.941118};
+  var position = {lat: 60.162786, lng: 24.932607};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
     center: position
@@ -12,10 +70,18 @@ function initMap() {
   */
 }
 
+/* blurraa kartan kun menu avataan */
 function openMenu() {
     document.getElementById("mobile-menu").style.width = "250px";
+    document.getElementById("map").style.filter = "blur(2px)";
+    document.getElementById("search-container").style.filter = "blur(2px)";
+    document.getElementById("title-container").style.filter = "blur(2px)";
 }
 
+/* palauttaa kartan takaisin normaaliksi kun menu suljetaan */
 function closeMenu() {
     document.getElementById("mobile-menu").style.width = "0";
+    document.getElementById("map").style.filter = "blur(0px)";
+    document.getElementById("search-container").style.filter = "blur(0px)";
+    document.getElementById("title-container").style.filter = "blur(0px)";
 }
