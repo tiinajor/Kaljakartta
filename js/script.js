93f102afebe8465a3f407a1851a055a0c9696dae
Mirko Nowak <microvvavve@gmail.com>
2017-08-31 18:43:38 +0300

    Added blue dot for user location and minor fixes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ window.onload = function(){
 	const tapButton = document.getElementById('tapButton');
 	const bottleButton = document.getElementById('bottleButton');
 	const mobileMenu = document.getElementById('mobile-menu');
+	const brandList = document.getElementById('brand');
+
+
+	let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
 
 
 	let beerBrands = ["karhu", "koff", "karjala", "lapin kulta", "ale cog", "heineken", "pirkka"];
@@ -13,16 +17,18 @@ window.onload = function(){
 
 	// hanat mukana haussa kyllä/ei
 	tapButton.onclick = function() {
-	tapButton.classList.toggle('selected');
+		tapButton.classList.toggle('selected');
 	};
 
 	// pullot mukana haussa kyllä/ei
 	bottleButton.onclick = function() {
-	bottleButton.classList.toggle('selected');
+		bottleButton.classList.toggle('selected');
 	};
 
+	document.getElementById('brand').addEventListener(touchEvent, openBeerList)
+
 	// menun avaus mobiilissa
-	document.getElementById('hamburger-menu').addEventListener("click", openMenu);
+	document.getElementById('hamburger-menu').addEventListener(touchEvent, openMenu);
 
 	noUiSlider.create(priceSlider, {
 		start: [ 0, 8.5 ],
@@ -93,6 +99,7 @@ function createList(brands, parentDiv) {
 	const div = document.createElement('div');
 	const ul = document.createElement('ul');
 	let li = document.createElement('li');
+	li.id = "brand";
 	let content = document.createTextNode("Merkki:");
 	li.appendChild(content);
 	ul.appendChild(li);
@@ -105,6 +112,7 @@ function createList(brands, parentDiv) {
 	}
 
 	ul.classList.add('beer-brand-list');
+	ul.id = "brand-list";
 	div.classList.add('centered');
 	div.classList.add('menu-block');
 	div.appendChild(ul);
@@ -119,21 +127,61 @@ function capitalizeFirstLetter(string) {
 
 /* luo kartan */
 function initMap() {
+
   	var position = {lat: 60.162786, lng: 24.932607};
   	var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
     center: position
   	});
-  	/*
-  	var marker = new google.maps.Marker({
-   		position: position,
-    	map: map
-  	});
-  	*/
+
+  	var bluedot = {
+	    url: 'http://i.imgur.com/WX8uTQ7.png',
+	    // This marker is 20 pixels wide by 32 pixels high.
+	    size: new google.maps.Size(20,20),
+	    // The origin for this image is (0, 0).
+	    origin: new google.maps.Point(0, 0),
+	    // The anchor for this image is the base of the flagpole at (0, 32).
+	    anchor: new google.maps.Point(10, 10)
+  	};
+
+  	if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+          	var marker = new google.maps.Marker({
+		   		position: pos,
+		    	map: map,
+		    	icon: bluedot
+		  	});
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+	'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+	infoWindow.open(map);
 }
 
 function toggleActive(id) {
   	document.getElementById(id).classList.toggle('selected');
+}
+
+
+function openBeerList() {
+	let list = document.getElementById('brand-list');
+	list.style.height = "150px";
+	list.style.overflowY = "scroll";
+	list.style.paddingRight = list.offsetWidth - list.clientWidth + "px";
 }
 
 
