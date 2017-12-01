8de056bd048d2c89ae6e60a501fd49d937dc1954
Kimi Talvitie <kimi.talvitie@hotmail.com>
2017-12-01 03:04:00 +0200

    Tweaked UI

--- js/barlist.js
+++ js/barlist.js
@@ -13,10 +13,15 @@ window.onload = function() {
   addButton.onclick = function() {
 
   // Add a new row to the table using the correct activityNumber
-      tracklistTable.innerHTML += '<tr class="input"><td id="counter">'+activityNumber+'</td><td><select class="input" required><option disabled selected value>Valitse juoma</option><option>Aura</option><option>Karhu III</option><option>Karhu IV</option><option>Karjala III</option><option>Karjala IV</option></select></td><td><input type="text" name="actlog1" class="input" placeholder="Syötä hinta" required></td><td><input type="text" name="time1" class="input" placeholder="Syötä koko" required></td><td><select class="input" required><option>Pullo</option><option>Hana</option></select></td></tr>';
+      tracklistTable.innerHTML += '<tr class="input"><td class="bev_number" id="counter">'+activityNumber+'</td><td><select class="input" required><option disabled selected value>Valitse juoma</option><option>Aura</option><option>Karhu III</option><option>Karhu IV</option><option>Karjala III</option><option>Karjala IV</option></select></td><td><input type="text" name="actlog1" class="input" placeholder="Syötä hinta" required></td><td><input type="text" name="time1" class="input" placeholder="Syötä koko" required></td><td><select class="input" required><option>Pullo</option><option>Hana</option></select></td><td class="img"><img src="kgps_icons/x.svg" alt="X"/></td></tr>';
 
     // Increment the activityNumber
     activityNumber += 1;
   }
 
+  var deleteBev = document.getElementById('img');
+
+  deleteBev.onclick = function(){
+    console.log("test");
+  }
 }
