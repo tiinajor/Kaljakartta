415f1e020a4340c0e81f58c88308f6dbdaf97892
Mirko Nowak <microvvavve@gmail.com>
2017-12-15 14:33:30 +0200

    Adding a row dynamically works

--- js/barlist.js
+++ js/barlist.js
@@ -1,31 +1,64 @@
-window.onload = function() {
+(function(window, document, undefined) {
+	let brandOptions;
+	const list = document.querySelector('ol');
+	const addBtn = document.querySelector('.addRow');
 
-  // Initialize the activityNumber
-var activityNumber = 2;
-var addButton = document.getElementById("add_beverage");
-var tracklistTable = document.getElementById("tracklist");
+	
+	getBrands().then(brands => brandOptions = createOptions(brands));
+	
+	addBtn.addEventListener('click', createInputRow);
 
 
-
-fetch("http://188.166.162.144:130/brands")
-	.then(response => {
-	return response.text();
-	})
-	.then(data => {
-		let beerBrands = data.slice(1, -1).split(",");
-		beerBrands = beerBrands.map(x => x.trim());
-		beerBrands.sort( function( a, b ) {
-			a = a.toLowerCase();
-			b = b.toLowerCase();
-			return a < b ? -1 : a > b ? 1 : 0;
-		});
-
-		let html = `<option disabled selected>Valitse Brändi</option>`;
-		for (let brand of beerBrands){
-			html += `<option value="${brand}">${brand}</option>`
-		}
-		document.getElementById("selectBrand").innerHTML = html;
-
-	})
-	.catch(err => console.log("Fetch Error: " + err));
+function createInputRow() {
+	const count = countFromTop();
+	console.log(brandOptions);
+	const row = `
+	<li>
+		<select class="selectBrand">${brandOptions}</select>
+		<input type="text" placeholder="Syötä hinta">
+		<input type="text" placeholder="Syötä koko">
+		<div class="selectServing">
+			<input type="radio" name="serving${count}" value="tap" id="tap${count}" checked>
+			<label class="disable-select clickable" for="tap${count}">Hana</label>
+			<input type="radio" name="serving${count}" value="bottle" id="bottle${count}">
+			<label class="disable-select clickable" for="bottle${count}">Pullo</label>
+		</div>
+		<button class="deleteItem">
+			<img src="kgps_icons/x.svg" alt="X">
+		</button>
+	</li>`;
+	list.innerHTML += row;
 }
+
+function createOptions(brands) {
+	let html = `<option disabled selected>Valitse Brändi</option>`;
+	for (let brand of brands){
+		html += `<option value="${brand}">${brand}</option>`
+	}
+	console.log(html);
+	return html;
+}
+
+function countFromTop() {
+	const items = list.querySelectorAll('li');
+	return items.length+1;
+}
+
+
+function getBrands() {
+	return new Promise((resolve, reject) => {
+		fetch("http://188.166.162.144:130/brands")
+			.then(response => {
+				return response.text();
+			})
+			.then(data => {
+				let beerBrands = data.slice(1, -1).split(",");
+				beerBrands = beerBrands.map(x => x.trim());
+				beerBrands.sort();
+				resolve(beerBrands);
+			})
+			.catch(error => reject(error));
+	});
+}
+
+})(window, document);
