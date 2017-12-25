70d37bf035b77b896a06e043c2ca1595fddb8fc2
Mirko Nowak <microvvavve@gmail.com>
2017-12-25 18:29:44 +0200

    Log out, edit row, save row work, edit serving does not

--- js/barlist.js
+++ js/barlist.js
@@ -1,49 +1,53 @@
 (function(window, document, undefined) {
 	let brandOptions;
-	const list = document.querySelector('ol');
+	const bar = window.sessionStorage.getItem('bar');
+	const table = document.querySelector('.beverages');
 	const addBtn = document.querySelector('.addRow');
+	const logOutBtn = document.querySelector('.logout');
 
+	console.log(bar);
+	getBarData(bar)
+		.then(data => {
+			const body = document.querySelector("tbody");
+			createBeerTableBody(body, data);
+			//updateTable(data.sort(sortBy("name", true)), globalVars.language);
+		})
+		.catch(error => console.log("getbardata " + error));
 	
 	getBrands().then(brands => brandOptions = createOptions(brands));
 	
 	addBtn.addEventListener('click', createInputRow);
+	logOutBtn.addEventListener('click', logOut);
 
 
 function createInputRow() {
-	const count = countFromTop();
-	console.log(brandOptions);
-	const row = `
-	<li>
-		<select class="selectBrand">${brandOptions}</select>
-		<input type="text" placeholder="Syötä hinta">
-		<input type="text" placeholder="Syötä koko">
-		<div class="selectServing">
-			<input type="radio" name="serving${count}" value="tap" id="tap${count}" checked>
-			<label class="disable-select clickable" for="tap${count}">Hana</label>
-			<input type="radio" name="serving${count}" value="bottle" id="bottle${count}">
-			<label class="disable-select clickable" for="bottle${count}">Pullo</label>
-		</div>
-		<button class="deleteItem">
-			<img src="kgps_icons/x.svg" alt="X">
-		</button>
-	</li>`;
-	list.innerHTML += row;
+	const deleteButton = document.createElement('td');
+	const row = document.createElement('tr');
+	deleteButton.innerHTML = `<img src="kgps_icons/x.svg" alt="X">`;
+	deleteButton.classList.add('column-s', 'delete', 'clickable');
+	deleteButton.addEventListener('click', (e) => deleteRow(e.target));
+	row.classList.add('editing');
+	row.innerHTML = `
+		<td class="column-xs"><img src="kgps_icons/beer-bottle.svg"></td>
+		<td class="column-l"><select class="selectBrand">${brandOptions}</select></td>
+		<td class="column-m"><input type="text" placeholder="Tyyppi"></td>
+		<td class="column-s"><input type="number" placeholder="L"></td>
+		<td class="column-s"><input type="number" placeholder="%"></td>
+		<td class="column-s"><input type="number" placeholder="€"></td>`;
+	row.appendChild(deleteButton);
+	table.appendChild(row);
 }
+
+
+
 
 function createOptions(brands) {
 	let html = `<option disabled selected>Valitse Brändi</option>`;
 	for (let brand of brands){
 		html += `<option value="${brand}">${brand}</option>`
 	}
-	console.log(html);
 	return html;
 }
-
-function countFromTop() {
-	const items = list.querySelectorAll('li');
-	return items.length+1;
-}
-
 
 function getBrands() {
 	return new Promise((resolve, reject) => {
@@ -59,6 +63,86 @@ function getBrands() {
 			})
 			.catch(error => reject(error));
 	});
+}
+
+function getBarData(barName) {
+	const url = "http://188.166.162.144:130/restaurant?name=" + barName.toLowerCase();
+	return fetch(url).then(response => response.status !== 500 ? response.json() : null).catch(error => showErrorMessage("getBarDataError: " + error));
+}
+
+
+function createBeerTableBody(tableBody, beers) {
+	let html = "";
+	for(let i=0;i<beers.length;i++) {
+		const beer = beers[i];
+		const name = capitalize(beer.name);
+		const type = capitalize(beer.type.split(',')[0]);
+		const bottleIcon = "kgps_icons/beer-bottle.svg";
+		const tapIcon = "kgps_icons/beer-tap.svg";
+		const icon = beer.serving === "tap" ? tapIcon : bottleIcon;
+		const iconAlt = beer.serving === "tap" ? 'Hana' : 'Pullo';
+		
+		const editButton = document.createElement('td');
+		editButton.innerHTML = `<img src="kgps_icons/edit.svg" alt="Muokkaa">`;
+		editButton.classList.add('column-s', 'edit', 'clickable');
+		editButton.addEventListener('click', (e) => editRow(e.target));
+
+		const row = document.createElement('tr');
+		row.innerHTML = `
+		<td class="column-xs"><img src=${icon} alt=${iconAlt}></td>
+		<td class="column-l"><input type="text" placeholder="Nimi" value="${name}" readonly></td>
+		<td class="column-m"><input type="text" placeholder="Tyyppi" value="${type}" readonly></td>
+		<td class="column-s"><input type="number" placeholder="L" value=${beer.vol} min="0" step="0.1" readonly></td>
+		<td class="column-s"><input type="number" placeholder="%" value=${beer.abv} min="0" step="0.1" readonly></td>
+		<td class="column-s"><input type="number" placeholder="€" value=${beer.price} min="0" step="0.1" readonly></td>`;
+		row.appendChild(editButton);
+		tableBody.appendChild(row);
+	}
+	
+	
+	const rows = document.getElementsByTagName("tr");
+	for (let i=0; i<rows.length; i++) {
+		if(i % 2 !== 0) {
+			rows[i].classList.add("odd-row");
+		}
+	}
+}
+
+function deleteRow(element) {
+	const clicked = element.classList.contains('delete') ? element : element.parentNode;
+	const currentRow = clicked.parentNode;
+	currentRow.parentNode.removeChild(currentRow);
+}
+
+function editRow(element) {
+	const clicked = element.classList.contains('edit') ? element : element.classList.contains('save') ? element : element.parentNode;
+	const currentRow = clicked.parentNode;
+	const cells = currentRow.querySelectorAll('td');
+	const editSaveButton = currentRow.lastChild;
+	const icon = editSaveButton.firstChild;
+	toggleInputs(cells);	
+	editSaveButton.classList.toggle('edit');
+	editSaveButton.classList.toggle('save');
+	icon.src = icon.src.includes("edit.svg") ? "kgps_icons/save.svg" : "kgps_icons/edit.svg";
+	icon.alt = icon.alt === "Muokkaa" ? "Tallenna muutokset" : "Muokkaa";
+}
+
+function toggleInputs(cells) {
+	for (let i=1; i<cells.length-1;i++) {
+		const input = cells[i].querySelector('input') || null;
+		input.readOnly = !input.readOnly;
+		input.classList.toggle('editable');
+	}
+}
+
+
+function logOut() {
+	window.location.replace('login.html');
+}
+
+
+function capitalize(string) {
+return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 })(window, document);
--- js/login.js
+++ js/login.js
@@ -1,34 +1,39 @@
-window.onload = function() {
-	const button = document.querySelector('.button-submit');
+(function(window, document, undefined) {
+	const form = document.querySelector('.login-form');
 	const usernameField = document.querySelector('input[name="username"]');
 	const passwordField = document.querySelector('input[name="password"]');
 	const usernameErrorField = document.querySelector('.invalid-user');
 	const passwordErrorField = document.querySelector('.invalid-pw');
-	let username;
-	let password;
 
-	button.addEventListener('click', (e) => {
+	form.addEventListener('submit', (e) => {
 		e.preventDefault();
-		console.log("click");
 
-		if(usernameField.checkValidity()) {
-			username = usernameField.value;
-			usernameErrorField.innerHTML = '';
-			usernameField.classList.remove('invalid');
-		} else {
+		if(!usernameField.checkValidity()) {
 			usernameErrorField.innerHTML = usernameField.validationMessage;
 			usernameField.classList.add('invalid');
 		}
 
-		if(passwordField.checkValidity()) {
-			password = passwordField.value;
-			passwordErrorField.innerHTML = '';
-			passwordField.classList.remove('invalid');
-		} else {
+		if(!passwordField.checkValidity()) {
 			passwordErrorField.innerHTML = passwordField.validationMessage;
 			passwordField.classList.add('invalid');
 		}
 
-		console.log({username, password});
+		logIn();
+		
 	})
-}
+
+
+	function logIn() {
+		usernameErrorField.innerHTML = '';
+		usernameField.classList.remove('invalid');
+		passwordErrorField.innerHTML = '';
+		passwordField.classList.remove('invalid');
+		window.sessionStorage.setItem('bar', usernameField.value);
+		window.location.replace('barlist.html');
+	}
+
+
+
+
+
+})(window, document);
